4d83f65c6b4117b2e5c53daf3040b99aaf331d72
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2024-09-30 16:38:12 -0700

    update pkg dependencies

---
+++ examples/vici_example.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from ftdi_serial import Serial
+from time import sleep
+from vicivalve import VICI
+
+# Uncomment for some prolific log statements.
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+logger.handlers[-1].setFormatter(
+   logging.Formatter(fmt='%(asctime)s:%(levelname)s: %(message)s'))
+
+
+ser = Serial("/dev/ttyUSB0", baudrate="9600")
+vici = VICI(serial=ser, positions=10)
+
+print(f"Current vici position is: {vici.current_position()}")
+vici.move_to_position(10)
+print(f"Current vici position is: {vici.current_position()}")
