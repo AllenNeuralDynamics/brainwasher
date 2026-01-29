37a9e06192879667d88d5964e36ca856c6c468fc
Micah Woodard <micah.woodard@alleninstitute.org>
2026-01-29 08:42:35 -0800

    uses cmd interface for tic

--- src/brainwasher/devices/pololu/pololu_tic_mixer.py
+++ src/brainwasher/devices/pololu/pololu_tic_mixer.py
@@ -1,5 +1,6 @@
 from brainwasher.devices.mixer import Mixer
-from ticlib import TicSerial
+import subprocess
+import logging
 
 MICROSTEP_FACTORS = {
     0: 1,
@@ -23,30 +24,92 @@ OPERATION_STATE = {
     "normal": 10
 }
 
+class TicCmd:
+    """Minimal ticcmd wrapper for USB control."""
+
+    def __init__(self, serial: str):
+        self.serial = serial
+
+    def _run(self, *args) -> str:
+        """
+        Send list of args to tic
+        
+        :param args: list of args for subprocess
+        """
+        
+        cmd = ["ticcmd", "--serial", self.serial, *args] 
+        return subprocess.check_output(cmd, text=True)
+    
+    def energize(self):
+        self._run("--energize")
+
+    def deenergize(self):
+        self._run("--deenergize")
+
+    def exit_safe_start(self):
+        self._run("--exit-safe-start")
+
+    def enter_safe_start(self):
+        self._run("--enter-safe-start")
+
+    def set_target_velocity(self, velocity: int | str):
+        
+        logging.debug(f"Setting velocity to {velocity}.")
+        self._run("--velocity", str(int(velocity)))
+
+    def _parse_status(self, starts_with: str) -> str:
+        """
+        Parse status message for specified line
+
+        :param starts_with: string line starts with 
+        """
+        out = self._run("--status")
+        for line in out.splitlines():
+            if line.strip().startswith(starts_with):
+                return line.split()[-1]
+        
+        raise RuntimeError(f"Could not determine status message line that starts with {starts_with}.")
+
+    def get_step_mode(self) -> int:
+        step_mode = int(self._parse_status("Step mode:"))
+        logging.debug(f"Step mode is {step_mode}.")
+        return step_mode
+
+    def get_target_velocity(self) -> int:
+        velocity = int(self._parse_status("Target velocity:"))
+        logging.debug(f"Target velocity is {velocity}.")
+        return velocity
+        
+
 class PololuTicMixer(Mixer):
     """An open loop mixing device."""
 
-    def __init__(self, max_rpm: float,
+    def __init__(self, serial:str, max_rpm: float,
                  min_rpm: float = 0,
                  steps_per_rev: int = 200,
                  name: str = None):
-        self.tic = TicSerial()
+        
+        self.tic = TicCmd(serial=serial)
         self.steps_per_rev = steps_per_rev
         super().__init__(min_rpm=min_rpm, max_rpm=max_rpm, name=name)
 
     def _set_mixing_speed(self, rpm: float):
         
         # convert rpms to microsteps per 10,000 secs
+        logging.debug(f"Setting rpm to {rpm}")
         microstep_mode = self.tic.get_step_mode()
         microsteps_per_rev = self.steps_per_rev * MICROSTEP_FACTORS[microstep_mode]
         steps_per_10000_secs = (rpm/60) * microsteps_per_rev * 10000
-        self.tic.set_target_velocity(steps_per_10000_secs)
+        logging.debug(f"rpm of {rpm} converted to {steps_per_10000_secs} steps/10000s.")
+        self.tic.set_target_velocity(round(steps_per_10000_secs))
 
     def _start_mixing(self):
-        if self.tic.get_target_velocity() == 0:  # don't start if speed is set to 0
+        if (v := self.tic.get_target_velocity()) != 0:  # don't start if speed is set to 0
+            logging.debug(f"Starting mixing as speed {v} steps.")
             self.tic.energize()
             self.tic.exit_safe_start()
     
     def _stop_mixing(self):
+        logging.debug("Stopping mixing.")
         self.tic.deenergize()
         self.tic.enter_safe_start()
