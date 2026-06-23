b7e162a6fe5c048db73fb9cc8ed3f98050261c17
hannalee2 <hanna.lee@alleninstitute.org>
2026-06-23 15:55:48 -0700

    Pass SlideContainer to SeqFlow

--- instruments/seqflow/src/seqflow/seqflow.py
+++ instruments/seqflow/src/seqflow/seqflow.py
@@ -1,16 +1,15 @@
 from mixology.instrument import Instrument
-from mixology.devices.simulated_devices.syringe_pump import SimSyringePump
+from mixology.devices.simulated_devices.peristaltic_pump import SimPeristalticPump
 from mixology.devices.simulated_devices.selector import SimSelector
 from seqflow.seqflow_models import (
     SeqFlowConfig,
     SeqFlowJob,
     SeqFlowJobStatus,
 )
-from mixology.devices.vessels import ReactionVessel, WasteVessel
-from threading import RLock, current_thread, Lock, Thread
+from mixology.devices.vessels import SlideContainer
 from functools import wraps
-from datetime import datetime, timedelta
-from typing import Literal, Union, Optional
+from datetime import datetime
+from typing import Union, Optional
 from pathlib import Path
 from time import perf_counter
 import yaml
@@ -26,29 +25,76 @@ class SeqFlow(Instrument):
     def __init__(
         self,
         config: SeqFlowConfig,
-        pump: SimSyringePump,
+        pump: SimPeristalticPump,
         selector: SimSelector,
-        job: Optional[SeqFlowJob] = None,
+        slide_container: SlideContainer,
     ):
         super().__init__()
         self.config = config
         self.pump = pump
         self.selector = selector
+
+        # Just two different nametags pointing to the exact same physical container in memory
+        # so the base Instrument class stays happy
+        self.slide_container = slide_container
+        self.rxn_vessel = self.slide_container
+
         # current job to run
-        self._job: SeqFlowJob = job
+        self._job: Optional[SeqFlowJob] = None
         self.job_status: SeqFlowJobStatus = SeqFlowJobStatus(status="idle")
 
     
-    def start_run(self):
+    def get_job(self) -> dict | None:
+        """
+        Convienence method to get current job
+        """
+        if self._job:
+            return self._job.model_dump()
+    
+    def set_job(self, job: Union[dict, SeqFlowJob]) -> None:
+        """
+        Convienence method to set current job
+
+        :param job: dict or SeqFlowJob object to set as current job
+
+        """
+        if self.job_status.status == "running":
+            self.log.warning("Cannot set job when instrument is running. Please pause.")
+            return
+
+        self._job = SeqFlowJob(**job) if type(job) == dict else job
+        status = "paused" if self._job.resume_state else "idle"
+        with self.job_status_lock:
+            self.log.info(f"Job set and setting to {status}")
+            self.job_status = SeqFlowJobStatus(status=status)
+
+    def start_run(self, job: SeqFlowJob):
         """
         Reset SeqFlow and start run
 
         :param job: job to run
 
         """
+        # validate and save job so instrument can run
+        valid_job = SeqFlowJob(**job)
+
+        # create path for job
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        job_path = Path(self.config.save_folder) / f"{valid_job.name}_{timestamp}.yaml"
+        job_path.parent.mkdir(parents=True, exist_ok=True)
+
+        valid_job.source_protocol.path = job_path
+        with open(Path(job_path), "w") as f:
+            yaml.dump(valid_job.model_dump(), f)
+        self.run(job_path)
         return {"message": "Starting run!"}
 
 
     def get_progress(self) -> dict:
         """Get current progress of job as a dict."""
         return {"job_status": self.job_status.status, "message": self.job_status.message}
+
+
+    def validate_job_against_instrument(self, job: SeqFlowJob):
+        """Validate that the job is compatible with the instrument."""
+        pass
