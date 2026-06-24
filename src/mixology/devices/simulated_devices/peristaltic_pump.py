7d2ad07ad047f65754954a4277c1e5b0fb99fab6
hannalee2 <hanna.lee@alleninstitute.org>
2026-06-24 15:05:44 -0700

    Change sim_pump function name

--- src/mixology/devices/simulated_devices/peristaltic_pump.py
+++ src/mixology/devices/simulated_devices/peristaltic_pump.py
@@ -1,14 +1,17 @@
 """Simulated Peristaltic Pump"""
 
 import logging
+from time import sleep
 
 class SimPeristalticPump:
-    def __init__(self, name: str):
+    def __init__(self, name: str = ""):
         logger_name = self.__class__.__name__ + (f".{name}" if name else "")
         self.log = logging.getLogger(logger_name)
 
-    def start(self, rate_ml_min: float):
-        self.log.debug(f"[{self.log.name}] Peristaltic pump started at {rate_ml_min} mL/min")
+    def dispense(self, volume_ml: float, rate_ml_min: float):
+        """Simulates dispensing a specific volume of fluid."""
+        self.log.info(f"[{self.log.name}] Dispensing {volume_ml} mL at {rate_ml_min} mL/min...")
 
-    def stop(self):
-        self.log.debug(f"[{self.log.name}] Peristaltic pump stopped.")
+        sleep(0.1) 
+        
+        self.log.debug(f"[{self.log.name}] Dispense complete.")
